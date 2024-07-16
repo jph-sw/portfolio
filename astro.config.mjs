ce2c0bdc3d30e3708d93b21fe329652b863b067a
jph-bsw <jph@patorg.de>
2024-07-16 12:28:48 +0200

    jph-add-react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://1π.com",
+  integrations: [tailwind(), react()],
+  site: "https://1π.com"
 });
